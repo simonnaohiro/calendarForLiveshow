d673a2fd53fe1cbfdd840cc082b300bb812f5b08
simonnaohiro <dreamtheater117@me.com>
2021-03-17 13:56:06 +0900

    add Line Bot Controllerr

--- app/Http/Controllers/LineBotController.php
+++ app/Http/Controllers/LineBotController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use LINE\LINEBot;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use LINE\LINEBot\Event\MessageEvent\TextMessage;
 
 class LineBotController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $lineAccessToken = config('services.line.access_token');
+        $lineChannelSecret = config('services.line.channel_secret');
+
+        $httpClient = new CurlHTTPClient($lineAccessToken);
+        $lineBot = new LINEBot($httpClient, ['channelSecret' => $lineChannelSecret]);
+
+        $signature = $request->header('x-line-signature');
+
+        if(!$lineBot->validateSignatuer($request->getContent(), $signature)) {
+            abort(400, 'Invalid signature');
+        }
+
+        $events = $lineBot->parseEventRequest($request->getContent(), $signature);
+    }
 }
